98fb97e41560333e2b8cfa5870181042866376bd
Henry W <henrywu847@gmail.com>
2025-09-13 22:59:42 +0000

    std modules

--- hi.cpp
+++ hi.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+import std;
 
 int main() {
-    std::cout << "hello world" << std::endl;
+    std::println("hello world!");
+
+    std::vector<int> v{5, 15, 25};
+    for (const auto& e : v)
+    {
+        std::println("{}", e);
+    }
 
     return 0;
 }
